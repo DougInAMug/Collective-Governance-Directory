675ddac68a29b214df625f3c5ab31ab7cef92907
Doug <dmhwebb@gmail.com>
2024-01-23 14:44:02 +0100

    rationalise data, finalise conversion script

--- static/csvToJSON.js
+++ static/csvToJSON.js
@@ -1,11 +1,25 @@
 import Papa from "papaparse";
 import fs from "node:fs";
 
-fs.readFile("static/CollectiveGovernanceDirectoryData.csv", "utf8", (err, data) => {
-  if (err) throw err;
-  const jsonString = JSON.stringify(Papa.parse(data, { header: true }));
-  fs.writeFile("static/CollectiveGovernanceDirectoryData.json", jsonString, (err) => {
+fs.readFile(
+  "static/CollectiveGovernanceDirectoryData.csv",
+  "utf8",
+  (err, data) => {
     if (err) throw err;
-    console.log("The file has been saved!");
-  });
-});
+    const jsonString = JSON.stringify(
+      Papa.parse(data, {
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+      })
+    );
+    fs.writeFile(
+      "static/CollectiveGovernanceDirectoryData.json",
+      jsonString,
+      (err) => {
+        if (err) throw err;
+        console.log("JSON generated without error");
+      }
+    );
+  }
+);
